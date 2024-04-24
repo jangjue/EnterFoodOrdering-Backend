383b51752435bc4eb8964685488bc7f31ce07ac6
jangjue <jangjue98@gmail.com>
2024-04-25 02:18:39 +0800

    Create restaturant form

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-//import { v2 as cloudinary } from "cloudinary";
-//import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 //import restaurantRoute from "./routes/RestaurantRoute";
 //import orderRoute from "./routes/OrderRoute";
 
@@ -12,13 +12,13 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
-{/* 
+ 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-*/}
+
 const app = express();
 
 app.use(cors());
@@ -32,7 +32,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
-//app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 //app.use("/api/restaurant", restaurantRoute);
 //app.use("/api/order", orderRoute);
 
