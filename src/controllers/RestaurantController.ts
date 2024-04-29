9744307f4c0ad73a23e416a0eb1b1c5cf61cada0
jangjue <jangjue98@gmail.com>
2024-04-29 08:55:33 +0800

    Add Order Features

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,91 +1,136 @@
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
+import cloudinary from "cloudinary";
+import mongoose from "mongoose";
+import Order from "../models/order";
 
-const getRestaurant = async (req: Request, res: Response) => {
+const getMyRestaurant = async (req: Request, res: Response) => {
   try {
-    const restaurantId = req.params.restaurantId;
-
-    const restaurant = await Restaurant.findById(restaurantId);
+    const restaurant = await Restaurant.findOne({ user: req.userId });
     if (!restaurant) {
       return res.status(404).json({ message: "restaurant not found" });
     }
-
     res.json(restaurant);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "something went wrong" });
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching restaurant" });
   }
 };
 
-const searchRestaurant = async (req: Request, res: Response) => {
+const createMyRestaurant = async (req: Request, res: Response) => {
   try {
-    const city = req.params.city;
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
 
-    const searchQuery = (req.query.searchQuery as string) || "";
-    const selectedCuisines = (req.query.selectedCuisines as string) || "";
-    const sortOption = (req.query.sortOption as string) || "lastUpdated";
-    const page = parseInt(req.query.page as string) || 1;
-
-    let query: any = {};
-
-    query["city"] = new RegExp(city, "i");
-    const cityCheck = await Restaurant.countDocuments(query);
-    if (cityCheck === 0) {
-      return res.status(404).json({
-        data: [],
-        pagination: {
-          total: 0,
-          page: 1,
-          pages: 1,
-        },
-      });
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists" });
     }
 
-    if (selectedCuisines) {
-      const cuisinesArray = selectedCuisines
-        .split(",")
-        .map((cuisine) => new RegExp(cuisine, "i"));
+    const imageUrl = await uploadImage(req.file as Express.Multer.File);
 
-      query["cuisines"] = { $all: cuisinesArray };
-    }
+    const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = imageUrl;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+    await restaurant.save();
 
-    if (searchQuery) {
-      const searchRegex = new RegExp(searchQuery, "i");
-      query["$or"] = [
-        { restaurantName: searchRegex },
-        { cuisines: { $in: [searchRegex] } },
-      ];
-    }
-
-    const pageSize = 10;
-    const skip = (page - 1) * pageSize;
-
-    // sortOption = "lastUpdated"
-    const restaurants = await Restaurant.find(query)
-      .sort({ [sortOption]: 1 })
-      .skip(skip)
-      .limit(pageSize)
-      .lean();
-
-    const total = await Restaurant.countDocuments(query);
-
-    const response = {
-      data: restaurants,
-      pagination: {
-        total,
-        page,
-        pages: Math.ceil(total / pageSize),
-      },
-    };
-
-    res.json(response);
+    res.status(201).send(restaurant);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
   }
 };
 
+const updateMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({
+      user: req.userId,
+    });
+
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    restaurant.restaurantName = req.body.restaurantName;
+    restaurant.city = req.body.city;
+    restaurant.country = req.body.country;
+    restaurant.deliveryPrice = req.body.deliveryPrice;
+    restaurant.estimatedDeliveryTime = req.body.estimatedDeliveryTime;
+    restaurant.cuisines = req.body.cuisines;
+    restaurant.menuItems = req.body.menuItems;
+    restaurant.lastUpdated = new Date();
+
+    if (req.file) {
+      const imageUrl = await uploadImage(req.file as Express.Multer.File);
+      restaurant.imageUrl = imageUrl;
+    }
+
+    await restaurant.save();
+    res.status(200).send(restaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const getMyRestaurantOrders = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
+
+const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: "order not found" });
+    }
+
+    const restaurant = await Restaurant.findById(order.restaurant);
+
+    if (restaurant?.user?._id.toString() !== req.userId) {
+      return res.status(401).send();
+    }
+
+    order.status = status;
+    await order.save();
+
+    res.status(200).json(order);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "unable to update order status" });
+  }
+};
+
+const uploadImage = async (file: Express.Multer.File) => {
+  const image = file;
+  const base64Image = Buffer.from(image.buffer).toString("base64");
+  const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+  const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+  return uploadResponse.url;
+};
+
 export default {
-  getRestaurant,
-  searchRestaurant,
+  updateOrderStatus,
+  getMyRestaurantOrders,
+  getMyRestaurant,
+  createMyRestaurant,
+  updateMyRestaurant,
 };
